3989821d4ded74245e1705595ac79d30c9c4f97b
MartinHeinz <martin7.heinz@gmail.com>
2023-03-05 14:43:35 +0100

    Go sample.

--- main.go
+++ main.go
@@ -2,32 +2,111 @@ package main
 
 import (
 	demo "github.com/saschagrunert/demo"
+	"github.com/urfave/cli/v2"
 )
 
+// TODO Test
 func main() {
 	d := demo.New()
 
-	d.Add(sample(), "demo-0", "just an example demo run")
+	d.Setup(setup)
+	d.Cleanup(cleanup)
+
+	d.Add(simple(), "simple", "Simple commands demo")
+	d.Add(longRunning(), "long-run", "Long-running commands demo")
+	d.Add(errorProne(), "error-prone", "Error-prone commands demo")
+	d.Add(longTyping(), "long-typing", "Commands that require a lot of typing")
 
 	d.Run()
 }
 
-func sample() *demo.Run {
+func simple() *demo.Run {
 	r := demo.NewRun(
-		"Demo Title",
-		"Some additional description",
+		"Simple commands demo",
 	)
 
 	r.Step(demo.S(
-		"A comment...",
+		"Simple commands:",
 	), nil)
 
-	// A single step can consist of a description and a command to be executed
-	r.Step(demo.S(
-		"Execute 'ps'...",
-	), demo.S(
-		"ps",
+	r.Step(nil, demo.S(
+		"ps aux",
+	))
+
+	r.Step(nil, demo.S(
+		"ls -l",
+	))
+
+	r.Step(nil, demo.S(
+		"cat some-file",
 	))
 
 	return r
+}
+
+func longRunning() *demo.Run {
+	r := demo.NewRun(
+		"Long-running commands demo",
+	)
+
+	r.Step(demo.S(
+		"Long running:",
+	), demo.S(
+		"docker build -t some-app .",
+	))
+
+	return r
+}
+
+func errorProne() *demo.Run {
+	r := demo.NewRun(
+		"Error-prone commands demo",
+	)
+
+	r.Step(demo.S(
+		"Error prone (dependant on external factors like network):",
+	), nil)
+
+	r.Step(nil, demo.S(
+		"curl --silent -X GET https://httpstat.us/418 -H \"Accept: application/json\" | jq .",
+	))
+
+	r.Step(nil, demo.S(
+		"curl -i -X POST \"https://httpbin.org/status/204\" --data '{\"some\":\"data\"}'",
+	))
+
+	r.Step(nil, demo.S(
+		"wget '-O some-file.txt https://raw.githubusercontent.com/octocat/Hello-World/master/README'",
+	))
+
+	return r
+}
+
+func longTyping() *demo.Run {
+	r := demo.NewRun(
+		"Commands that require a lot of typing",
+	)
+
+	r.Step(demo.S(
+		"Hard to type:",
+	), demo.S(
+		"openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes ",
+		"-keyout example.key -out example.crt -subj \"/CN=example.com\"",
+		"-addext \"subjectAltName=DNS:example.com,DNS:www.example.net,IP:10.0.0.1\"",
+	))
+
+	return r
+}
+
+// setup will run before every demo
+func setup(ctx *cli.Context) error {
+	// Ensure can be used for easy sequential command execution
+	return demo.Ensure(
+		"echo 'Here\nis\nsome\ndata\nfor\nyou' > some-file",
+	)
+}
+
+// setup will run after every demo
+func cleanup(ctx *cli.Context) error {
+	return demo.Ensure("rm some-file")
 }
